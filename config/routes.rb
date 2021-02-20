5e7c7216fdb5e9dac012bb994d03f25444230654
sgarrick <sgarrick@uchicago.edu>
2021-02-20 17:06:54 +0000

    user details looks good

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,8 @@ Rails.application.routes.draw do
 
 get("/users", { :controller => "users", :action => "index"})
 
+get("/users/:path_username", {:controller => "users", :action => "show"})
+
+get("/photos")
 
 end
